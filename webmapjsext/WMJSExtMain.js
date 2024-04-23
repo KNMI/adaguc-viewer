2efad4da061c6ebfb5c6043d70b9c6cd4e93bd57
vorticeAemet <vorticeAemet@aemet.es>
2024-04-23 09:25:21 +0000

    VERSION AEMET 1.0.3

--- webmapjsext/WMJSExtMain.js
+++ webmapjsext/WMJSExtMain.js
@@ -5,7 +5,7 @@ var error = function (e) {
 var debug = function (e) {
   console.log(e);
 };
-var versionInfo = "ADAGUC viewer version AEMET 1.0.1";
+var versionInfo = "ADAGUC viewer version AEMET 1.0.3";
 var mainWebmapJS;
 
 function decimalFormatter(input, width) {
